812b2f8eae7884686db425f4d8d4f60fc1221b3e
Matias-Miranda-Uade <mamiranda@uade.edu.ar>
2025-10-28 14:45:39 -0300

    BFS y cambios NodeEntity

--- src/main/java/com/example/demo/model/NodeEntity.java
+++ src/main/java/com/example/demo/model/NodeEntity.java
@@ -2,12 +2,20 @@ package com.example.demo.model;
 
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Node
 public class NodeEntity {
 
     @Id
     private Integer esquinaId;
+    private String nombre;
+
+    @Relationship(type = "ROAD")
+    private List<RoadRelationship> roads = new ArrayList<>();
 
     public NodeEntity() {}
 
@@ -15,12 +23,29 @@ public class NodeEntity {
         this.esquinaId = esquinaId;
     }
 
+    public NodeEntity (Integer esquinaId, String nombre) {
+        this.esquinaId = esquinaId;
+        this.nombre = nombre;
+    }
+
     public Integer getEsquinaId() {
         return esquinaId;
     }
-
-    public void setEsquinaId(Integer esquinaId) {
+    public void setEsquinaId(Integer esquinaId){
         this.esquinaId = esquinaId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public List<RoadRelationship> getRoads() {
+        return roads;
+    }
+    public void setRoads(List<RoadRelationship> roads) {
+        this.roads = roads;
     }
 }
 
